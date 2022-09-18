24b44694531f04006a8c247017673d3a74d10be8
myl <myldast1@126.com>
2022-09-18 23:52:06 +0800

    Future study:https://book.flutterchina.club/chapter1/dart.html#_1-4-4-%E5%BC%82%E6%AD%A5%E6%94%AF%E6%8C%81

--- startup_woop/lib/main.dart
+++ startup_woop/lib/main.dart
@@ -2,17 +2,56 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//vscode只提示/后面，android studio 🐂🍺
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
 
-//MARK: 格式化代码快捷键：全选，然后依次按下 cmd+k, cmd+f
-void main() => runApp(MyApp());
+void testFutureDalay() {
+
+  Future.delayed(Duration(seconds: 2),(){
+    return "hi world!";
+    // throw AssertionError("Error");
+  }).then((data){
+    //执行成功会走到这里
+    print(data);
+  }).catchError((e){
+    //执行失败会走到这里
+    print(e);
+  }).whenComplete((){
+    //无论成功或失败都会走到这里
+    print("whenCom");
+  });
+}
+
+void testFutureWait() {
+  Future.wait([
+    Future.delayed(Duration(seconds: 2),(){
+      return "delay 2s";
+    }),
+    Future.delayed(Duration(seconds: 4),(){
+      // return "delay 4s";
+      throw AssertionError("err!!!");
+    })
+  ]).then((value){
+    final res = value[0] + "," + value[1];
+    print(res);
+  }).catchError((err){
+    print(err);
+  }).whenComplete(() {
+    print("whencom");
+  });
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp();
+
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
+    // testFutureDalay();
+    testFutureWait();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -20,7 +59,6 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          // child: Text('Hello World11'),
           child: Text(wordPair.asPascalCase),
         ),
       ),
